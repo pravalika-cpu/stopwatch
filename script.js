29988a9cde41a7102c9c2c55451c145286559061
pravalika-cpu <65751881+pravalika-cpu@users.noreply.github.com>
2020-06-13 17:16:11 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,6 +1,8 @@
 let seconds=0;
 let minutes=0;
 let hours=0;
+let interval=null;
+let status="stopped"
 function stopwatch(){
     seconds++
     if(seconds/60===1){
@@ -14,4 +16,25 @@ hours++;
 }
 document.getElementById("display").innerHTML=hours+":"+minutes+":"+seconds;
 }
-window.setInterval(stopwatch,1 )
+
+function startstop(){
+if(status==="stopped"){
+interval=window.setInterval(stopwatch,1000);
+document.getElementById("startstop").innerHTML="stop";
+status="started"
+}
+else{
+    window.clearInterval(interval);
+    document.getElementById("startstop").innerHTML="start"
+    status="stopped"
+
+}
+}
+function reset(){
+    window.clearInterval(interval)
+    seconds=0;
+    minutes=0;
+    hours=0;
+    document.getElementById("display").innerHTML=00:00:00;
+    document.getElementById("startstop").innerHTML="start"
+}

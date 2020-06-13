0b26ab26f34930831754a3137d72b00ea29bc5c1
pravalika-cpu <65751881+pravalika-cpu@users.noreply.github.com>
2020-06-13 16:47:19 +0530

    Create script.js

---
+++ script.js
@@ -0,0 +1,17 @@
+let seconds=0;
+let minutes=0;
+let hours=0;
+function stopwatch(){
+    seconds++
+    if(seconds/60===1){
+    seconds=0;
+    minutes++;
+    if(minutes/60==1){
+minutes=0;
+hours++;
+    }
+    
+}
+document.getElementById("display").innerHTML=hours+":"+minutes+":"+seconds;
+}
+window.setInterval(stopwatch,10)
